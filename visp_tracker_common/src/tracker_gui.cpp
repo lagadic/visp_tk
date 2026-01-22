5f992c1dbe2479fdb077074c5bc61c57bcf2c4ae
LAGNEAU Romain <romain.lagneau@inria.fr>
2026-01-22 10:30:42 +0100

    [CORE] Handling signals in tracker_gui node

--- visp_tracker_common/src/tracker_gui.cpp
+++ visp_tracker_common/src/tracker_gui.cpp
@@ -1,12 +1,29 @@
+#include <csignal>
+
 #include <rclcpp/rclcpp.hpp>
 
 #include <visp_tracker_common/TrackerGUI.hpp>
 
+std::shared_ptr<visp_tracker_common::TrackerGUI> gui;
+
+void signalHandler(int signum)
+{
+  // Send a null velocity to the robot and set its state to quit
+  if (gui) {
+    gui->quit();
+  }
+  RCLCPP_WARN(gui->get_logger(), "Sent termination signal to the node due to signal %d", signum);
+}
+
 int main(int argc, char *argv[])
 {
+  signal(SIGABRT, signalHandler);
+  signal(SIGINT, signalHandler);
+  signal(SIGTERM, signalHandler);
+
   rclcpp::init(argc, argv);
-  std::shared_ptr<visp_tracker_common::TrackerGUI> gui = std::make_shared<visp_tracker_common::TrackerGUI>("tracker_gui");
-  bool status = gui->init();
+  gui = std::make_shared<visp_tracker_common::TrackerGUI>("tracker_gui");
+  bool status = gui->init(gui);
   if (status) {
     while (gui->has_to_run()) {
       rclcpp::spin_some(gui);
