81f388eb2a266cf9b0af72c2f65da4fbb3c274e2
Fabien Spindler <fabien.spindler@inria.fr>
2026-03-04 16:06:58 +0100

    Update or add copyright header

--- visp_apriltag/src/AprilTagTracker.cpp
+++ visp_apriltag/src/AprilTagTracker.cpp
@@ -1,3 +1,27 @@
+/*
+ * Copyright (C) 2026 by Inria. All rights reserved.
+ *
+ * This software is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file LICENSE.txt at the root directory of this source
+ * distribution for additional information about the GNU GPL.
+ *
+ * For using this software that can not be combined with the GNU
+ * GPL, or if you have questions regarding the use of this file,
+ * please contact Inria at visp@inria.fr
+ *
+ * This software was developed at:
+ * Inria centre at Rennes University
+ * Campus Universitaire de Beaulieu
+ * 35042 Rennes Cedex
+ * France
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 #include <visp_apriltag/AprilTagTracker.hpp>
 
 namespace visp_apriltag
