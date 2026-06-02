9011cc997af46d73b92d6216320bc0b055b627ec
Fabien Spindler <fabien.spindler@inria.fr>
2026-06-02 08:34:47 +0200

    Fiw warning around deprecated headers inclusion

--- visp_tracker_common/include/visp_tracker_common/BaseTracker.hpp
+++ visp_tracker_common/include/visp_tracker_common/BaseTracker.hpp
@@ -32,8 +32,8 @@
 #include <std_srvs/srv/trigger.hpp>
 
 #include <tf2/exceptions.hpp>
-#include <tf2_ros/transform_listener.h>
-#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.hpp>
+#include <tf2_ros/buffer.hpp>
 
 #include <visp_common/camera.hpp>
 #include <visp_common/path_retriever.hpp>
