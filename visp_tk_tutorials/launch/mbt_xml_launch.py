940a5f2835ad78cfb4784fb40deb157f6cd3aa27
LAGNEAU Romain <romain.lagneau@inria.fr>
2026-06-24 14:43:46 +0200

    [FIX] Fixed MBT tutorials for Jazzy: wrong bag names

--- visp_tk_tutorials/launch/mbt_xml_launch.py
+++ visp_tk_tutorials/launch/mbt_xml_launch.py
@@ -104,7 +104,7 @@ def prepare_parameters(context):
 
     # Rosbag player
     bag_folder = PathJoinSubstitution(
-        [FindPackageShare("visp_tk_tutorials"), "bag", "tutorial-static-box-humble"]
+        [FindPackageShare("visp_tk_tutorials"), "bag", "tutorial-static-box-jazzy"]
     )
     bag_player = ExecuteProcess(
         cmd=[
